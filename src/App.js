8a481c10bbe7485f2a70227d81e66a408a434a58
guidomora <guidomorabito161@hotmail.com>
2022-10-26 20:17:47 -0300

    nada

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import './App.css';
 import Container from './Components/Container';
 
--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -4,7 +4,11 @@ import "../Styles/Nav.css"
 const Nav = () => {
     return (
         <div className="nav">
-
+            <ul>
+                <li>Inicio</li>
+                <li>Turnos</li>
+                <li>Calculador</li>
+            </ul>
         </div>
     )
 };
