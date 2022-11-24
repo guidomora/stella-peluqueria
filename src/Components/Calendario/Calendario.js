17617a5b5efde3f2b07a0dd1fc9f64dd24d40760
guidomora <guidomorabito161@hotmail.com>
2022-11-24 11:44:38 -0300

    sigue sin funcionar el calendario

--- src/Components/Calendario/Calendario.js
+++ src/Components/Calendario/Calendario.js
@@ -7,19 +7,34 @@ import { Calendar, dateFnsLocalizer } from "react-big-calendar";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import "react-datepicker/dist/react-datepicker.css";
 import "./Calendario.css";
+import { doc, setDoc, collection, query, onSnapshot } from "firebase/firestore";
+import { db } from "../Firebase";
+import { v4 as uuidv4 } from "uuid";
+import { json } from "react-router-dom";
 
-const eventos = [];
+
 const Calendario = ({ nombre }) => {
   const [allEvents, setAllEvents] = useState([]);
 
   const handleSelectSlot = useCallback(
     ({ start, end }) => {
+      const id = uuidv4();
       const title = window.prompt("Turno:");
       if (title) {
-        setAllEvents((prev) => [...prev, { start, end, title }]);
+        setAllEvents((prev) => [...prev, { start, end, title, id, allDay:false }])
+        const agregarFirestore = async () => {
+          await setDoc(doc(db, "Turnos", id), {
+            title: title,
+            start: JSON.stringify(start),
+            end: JSON.stringify(end),
+            allDay:false
+          });
+        }
+        agregarFirestore()
       }
     },
-    [allEvents]
+    [allEvents],
+    console.log( "hola", allEvents)
   );
 
   const locales = {
@@ -34,24 +49,40 @@ const Calendario = ({ nombre }) => {
     locales,
   });
 
-  const saveData = () => {
-    localStorage.setItem("turnos", JSON.stringify(allEvents));
-    console.log("guardado");
+  // const saveData = () => {
+  //   localStorage.setItem("turnos", JSON.stringify(allEvents));
+  //   console.log("guardado");
+  // };
+
+  // useEffect(() => {
+  //   const items = JSON.parse(localStorage.getItem("turnos"));
+  //   if (items) {
+  //     setAllEvents(items);
+  //   }
+  // }, []);
+
+  const obtenerServicios = async () => {
+    const q = query(collection(db, "Turnos"));
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      const docs = [];
+      querySnapshot.forEach((doc) => {
+        docs.push({ ...doc.data() });
+      });
+      setAllEvents(docs);
+    });
+    unsubscribe()
   };
 
   useEffect(() => {
-    const items = JSON.parse(localStorage.getItem("turnos"));
-    if (items) {
-      setAllEvents(items);
-    }
-  }, []);
+    obtenerServicios()
+  }, [])
 
-  console.log(allEvents);
+  console.log("nuevo", allEvents);
 
   return (
     <div>
       <h1>{nombre}</h1>
-      <button onClick={() => saveData()}> Guardar</button>
+      {/* <button onClick={() => saveData()}> Guardar</button> */}
       <Calendar
         localizer={localizer}
         events={allEvents}
