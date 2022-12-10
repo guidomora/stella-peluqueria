a1c55ee7315afd26a5c133b80b7071fd19bdf63a
guidomora <guidomorabito161@hotmail.com>
2022-12-10 00:29:33 -0300

    nothing

--- src/Components/Calendario/Calendario.js
+++ src/Components/Calendario/Calendario.js
@@ -18,9 +18,6 @@ import {
   addDoc,
 } from "firebase/firestore";
 
-
-
-
 const Calendario = ({ nombre, turno }) => {
   const [allEvents, setAllEvents] = useState([]);
 
@@ -29,8 +26,7 @@ const Calendario = ({ nombre, turno }) => {
       const id = uuidv4();
       const title = window.prompt("Turno:");
       if (title) {
-        setAllEvents((prev) => [...prev,{ start,
-          end, allDay, title, id }]);
+        setAllEvents((prev) => [...prev, { start, end, allDay, title, id }]);
       }
     },
     [allEvents]
@@ -47,61 +43,70 @@ const Calendario = ({ nombre, turno }) => {
     locales,
   });
 
-// Save the data to localStorage
+  // Save the data to localStorage
   // const saveData = () => {
   //   localStorage.setItem(`${turno}`, JSON.stringify(allEvents));
   //   console.log("guardado");
   // };
 
-// Get the data from localStorage
-// const items = JSON.parse(localStorage.getItem(`${turno}`));
-// if (items) {
-//   const events = items.map(item => ({
-//    ...item,
-//    start: new Date(item.start),
-//    end: new Date(item.end)
-//   }))
-//   setAllEvents(events);
-// }
+  // Get the data from localStorage
+  // const items = JSON.parse(localStorage.getItem(`${turno}`));
+  // if (items) {
+  //   const events = items.map(item => ({
+  //    ...item,
+  //    start: new Date(item.start),
+  //    end: new Date(item.end)
+  //   }))
+  //   setAllEvents(events);
+  // }
 
-const saveData = async (event) => {
-  const updatedServices = [event, ...allEvents];
-    setAllEvents(updatedServices);
-  const docRef = await addDoc(collection(db, "turno-tobi"), {
-      allDay: event.allDay,
-      start: event.start,
-      end: event.end,
-      id: event.id,
-      title: event.title
-    });
-    console.log(docRef)
-}
+  // const saveData = async (event) => {
+  //   const updatedServices = [event, ...allEvents];
+  //     setAllEvents(updatedServices);
+  //   const docRef = await addDoc(collection(db, "turno-tobi"), {
+  //       allDay: event.allDay,
+  //       start: event.start,
+  //       end: event.end,
+  //       id: event.id,
+  //       title: event.title
+  //     });
+  //     console.log(docRef)
+  // }
 
+  const ver = () => {
+    const saveData = async () => {
+      const mapeo2 = allEvents.map((eventos) => eventos);
+      await setDoc(doc(db, "turno-tobi", mapeo2.id), {
+        allDay: mapeo2.allDay,
+        start: mapeo2.start,
+        end: mapeo2.end,
+        id: mapeo2.id,
+        title: mapeo2.title,
+      });
+      console.log(mapeo2);
+    };
+    saveData()
+  };
+  ver();
 
-const obtenerDatos = () => {
+  const obtenerDatos = () => {};
 
- }
-
-
- const borrarTurno = () => {
-  localStorage.removeItem(`${turno}`)
- }
-  
+  const borrarTurno = () => {
+    localStorage.removeItem(`${turno}`);
+  };
 
   useEffect(() => {
-    obtenerDatos(allEvents)
+    obtenerDatos(allEvents);
   }, []);
 
   console.log(allEvents);
-
-
 
   return (
     <div>
       <div className="calendar-subContainer">
         <h2>{nombre}</h2>
         <div className="guardado">
-          <p className="save-text" onClick={() => saveData()}> Guardar turno</p>
+          <p className="save-text"> Guardar turno</p>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="27"
@@ -113,7 +118,11 @@ const obtenerDatos = () => {
             <path d="M9.828.722a.5.5 0 0 1 .354.146l4.95 4.95a.5.5 0 0 1 0 .707c-.48.48-1.072.588-1.503.588-.177 0-.335-.018-.46-.039l-3.134 3.134a5.927 5.927 0 0 1 .16 1.013c.046.702-.032 1.687-.72 2.375a.5.5 0 0 1-.707 0l-2.829-2.828-3.182 3.182c-.195.195-1.219.902-1.414.707-.195-.195.512-1.22.707-1.414l3.182-3.182-2.828-2.829a.5.5 0 0 1 0-.707c.688-.688 1.673-.767 2.375-.72a5.922 5.922 0 0 1 1.013.16l3.134-3.133a2.772 2.772 0 0 1-.04-.461c0-.43.108-1.022.589-1.503a.5.5 0 0 1 .353-.146zm.122 2.112v-.002.002zm0-.002v.002a.5.5 0 0 1-.122.51L6.293 6.878a.5.5 0 0 1-.511.12H5.78l-.014-.004a4.507 4.507 0 0 0-.288-.076 4.922 4.922 0 0 0-.765-.116c-.422-.028-.836.008-1.175.15l5.51 5.509c.141-.34.177-.753.149-1.175a4.924 4.924 0 0 0-.192-1.054l-.004-.013v-.001a.5.5 0 0 1 .12-.512l3.536-3.535a.5.5 0 0 1 .532-.115l.096.022c.087.017.208.034.344.034.114 0 .23-.011.343-.04L9.927 2.028c-.029.113-.04.23-.04.343a1.779 1.779 0 0 0 .062.46z" />
           </svg>
         </div>
-        <button type="button" className="btn btn-danger delete-button" onClick={() => borrarTurno()}>
+        <button
+          type="button"
+          className="btn btn-danger delete-button"
+          onClick={() => borrarTurno()}
+        >
           Eliminar
           <svg
             xmlns="http://www.w3.org/2000/svg"
